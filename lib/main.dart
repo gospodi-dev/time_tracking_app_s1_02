b4ef5da5d7f64971e6ded77b1c5cbef4162da5f2
Gospodi <gospodijazzet@yandex.ru>
2021-10-27 19:15:29 +0300

    add file

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+import 'time_tracking_main_page.dart';
 
+class TimeTrackingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TimeTrackingMainPage(),
+    );
+  }
+}
